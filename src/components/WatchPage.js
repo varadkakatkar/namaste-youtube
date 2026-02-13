bd4c62c3214dd8de7186c9d10cc997e1c1c5103b
Varad Kakatkar <varadkakatkar@gmail.com>
2026-02-13 17:51:31 +0530

    intermediate commit

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,21 +1,35 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { closeMenu } from '../utils/appSlice';
-import { useSearchParams } from 'react-router-dom';
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"))
-  useEffect(() => {
-    dispatch(closeMenu)
-  }, [])
-  return (
-    <div className="flex justify-center">
-        <iframe width="1000" height="500" src={`https://www.youtube.com/embed/${searchParams.get("v")}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
-  )
-}
+  const videoId = searchParams.get("v");
 
-export default WatchPage
+  useEffect(() => {
+    dispatch(closeMenu);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (videoId) {
+      document.title = `YouTube - ${videoId}`;
+    }
+  }, [videoId]);
+  return (
+    <div className="flex justify-center m-10 p-10 w-full">
+      <iframe
+        width="1000"
+        height="500"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
